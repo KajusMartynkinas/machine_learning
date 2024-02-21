84704a6660a5b266fcf1efa0430806eba6d4d8ec
KajusMartynkinas <61082665+KajusMartynkinas@users.noreply.github.com>
2024-02-21 14:32:46 +0200

    Start

---
+++ e_commerce.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+
+df = pd.read_csv('ecommerce.csv')
+# print(df.head())
+print(df.info())
+print(df.describe())
+
+# EDA
+sns.jointplot(x='Time on Website', y='Yearly Amount Spent', data=df, alpha=0.5)
+plt.show()
+
+sns.jointplot(x='Time on App', y='Yearly Amount Spent', data=df, alpha=0.5)
+plt.show()
+
+sns.pairplot(df, kind = 'scatter', plot_kws={'alpha': 0.4})
+plt.show()
